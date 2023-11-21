f3125b2006006ac20e43766798ee31c56236a17a
feche2022 <feche09_09@hotmail.com>
2023-11-21 09:24:40 -0300

    Primer App del curso Desarrollo de aplicaciones en "CODERHOUSE"

--- App.js
+++ App.js
@@ -4,7 +4,11 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.greeting}>Hola, Coder!</Text>
+      <Text style={styles.description}>Despues de varios intentos y de casi quemar la compu, logre ver la aplicacion en tiempo real jaja!</Text>
+      <Text style={styles.description}>Ah, y también apliqué estos estilos.</Text>
+      <Text style={styles.description}>Sin más que agregar, denme el certificado 😎.</Text>
+      <Text style={styles.emoji}>💥</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +17,25 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    textAlign: 'center',
+    backgroundColor: '#7ED7C1'
+  },
+  greeting: {
+    fontSize: 32,
+    fontWeight: 'bold',
+    marginBottom: 10,
+    color: '#fff',
+  },
+  description: {
+    fontSize: 18,
+    textAlign: 'center',
+    marginHorizontal: 30,
+    margin: 15,
+    color: '#B31312',
+  },
+  emoji: {
+    fontSize: 100,
   },
 });
